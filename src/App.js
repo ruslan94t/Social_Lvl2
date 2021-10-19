cd7742c1e162e448c58aaa2bf27977164c61c175
ruslan94t <p3triri@yandex.ru>
2021-10-19 23:23:28 +0300

    07 - what about component

--- src/App.js
+++ src/App.js
@@ -3,14 +3,29 @@ import './App.css';
 const App = () => {
     return (
         <div>
-            <div>onde div</div>
-           <ul>
-               <li>css</li>
-               <li>react</li>
-               <li>js</li>
-           </ul>
+            <Header />
+            <Technologies />
+
         </div>
     )
 }
 
+const Technologies = ()=>{
+    return(
+        <ul>
+            <li>css</li>
+            <li>react</li>
+            <li>js</li>
+        </ul>
+    )
+}
+const Header = ()=> {
+    return(
+        <div>
+            <a href="#">Home</a>
+            <a href="#">New fead</a>
+            <a href="#">Message</a>
+        </div>
+    )
+}
 export default App;
