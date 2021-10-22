807ac54545b10cdfd87a91c0dbf21ebc8b9c51d7
ruslan94t <p3triri@yandex.ru>
2021-10-22 23:50:10 +0300

    18 Dialogs Page wrapper

---
+++ src/components/Dialogs/Dialogs.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import s from './Dialogs.module.css';
+
+
+
+function Dialogs(props) {
+    return (
+        <div>
+          Dialogs
+        </div>
+    );
+}
+
+export default Dialogs;
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -3,7 +3,7 @@ import s from './Profile.module.css'
 import MyPosts from "./MyPosts/MyPosts";
 function Profile(props) {
     return (
-        <div className={s.content}>
+        <div>
             <div>
                 <img src='https://pinster.ru/cache/4e6bd3a7/avd56a9538548e2dbcd98.jpeg' />
             </div>
