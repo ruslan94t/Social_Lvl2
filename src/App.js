ee56e1c400dc374b9e502665cc7fb4bd1cce51fe
ruslan94t <p3triri@yandex.ru>
2021-10-22 17:14:08 +0300

    11 style css grid morofed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-
-
-
 import React from 'react';
 import './App.css';
-import Header from "./Header";
-import Technologies from "./Technologies";
-import Footer from "./Footer";
 
 const App = () => {
     return (
-        <div>
-            <Header />
+        <div className='app-wrapper'>
+            <header className='header'>
+                <img src='https://upload.wikimedia.org/wikipedia/commons/1/1e/RPC-JP_Logo.png'/>
+            </header>
 
-            <Technologies />
-           <Footer />
+            <nav className='nav'>
+
+                <div>
+                   <a href="#">Profile</a>
+                </div>
+                <div>
+                    <a href="#">Messages</a>
+                </div>
+                <div>
+                    <a href="#">News</a>
+                </div>
+                <div>
+                    <a href="#">Music</a>
+                </div>
+                <div>
+                    <a href="#">Settings</a>
+                </div>
+            </nav>
+            <div className='content'>
+                <div>
+                    <img src='https://pinster.ru/cache/4e6bd3a7/avd56a9538548e2dbcd98.jpeg' />
+                </div>
+                <div>ava+description</div>
+                <div>
+                    My Post
+
+                <div>New Post</div>
+                 <div>
+                     <div>Post</div>
+                     <div>Post 2</div>
+                     <div>Post 3</div>
+                     <div>Post 4</div>
+                 </div>
+                </div>
+            </div>
+
 
         </div>
     )
 }
-
 
 
 export default App;
