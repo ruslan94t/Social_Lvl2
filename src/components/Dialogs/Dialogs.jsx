9a0526c6cfbe7619d05a415a35929adf285c2b17
ruslan94t <p3triri@yandex.ru>
2021-10-25 01:00:30 +0300

    21 css Morofet

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,11 +2,40 @@ import React from 'react';
 import s from './Dialogs.module.css';
 
 
-
 function Dialogs(props) {
     return (
-        <div>
-          Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog+' '+s.active}>
+                    Dimych
+                </div>
+                <div className={s.dialog}>
+                    Andrey
+                </div>
+                <div className={s.dialog}>
+                    Sveta
+                </div>
+                <div className={s.dialog}>
+                    Sasha
+                </div>
+                <div className={s.dialog}>
+                    Valera
+                </div>
+                <div className={s.dialog}>
+                    Masha
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>
+                    Hi
+                </div>
+                <div className={s.message}>
+                    How is Your
+                </div>
+                <div className={s.message}>
+                    Fine what is
+                </div>
+            </div>
         </div>
     );
 }
