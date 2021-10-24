0d1d0d012e962b399c2c6b53ae361cb2b0d72ad7
ruslan94t <p3triri@yandex.ru>
2021-10-25 00:26:00 +0300

    20 Navlink activClass

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import s from './Navbar.module.css'
+import {NavLink} from "react-router-dom";
 function Navbar(props) {
     return (
         <nav className={s.nav}>
 
             <div className={s.item}>
-                <a >Profile</a>
+                <NavLink to="/profile" activeClassName={s.active} >Profile</NavLink>
             </div>
             <div className={s.item}>
-                <a >Messages</a>
+                <NavLink to="/dialogs" activeClassName={s.active} >Messages</NavLink>
             </div>
             <div className={`${s.item} ${s.active}`}>
                 <a >News</a>
