1ebb47d118d430d65ee273e5e2c10b3a44b75099
ruslan94t <p3triri@yandex.ru>
2021-10-28 17:19:49 +0300

    26 Bll and ui clear Profile

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,20 +1,10 @@
 import React from 'react';
 import s from './Dialogs.module.css';
-import {NavLink} from "react-router-dom";
+import DialogItem from "./DialogItem/Dialogs";
+import Message from "./Message/Message";
 
 
-const DialogItem =(props)=>{
-    return     <div className={s.dialog}>
-        <NavLink to={'/dialogs/'+props.id}>
-            {props.name}
-        </NavLink>
 
-    </div>
-}
-
-const Message =(props)=>{
-    return <div className={s.message}>{props.message}</div>
-}
 
 let dialogs = [
     {id:1, name:"Alina"},
