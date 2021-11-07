d1d035b43dd40ccc231605dda4014fcacde449d1
ruslan94t <p3triri@yandex.ru>
2021-11-07 23:49:56 +0300

    41 Refactor reducer

---
+++ src/redux/dialogs-reducer.js
@@ -0,0 +1,34 @@
+const UPDATE_NEW_MESSAGE_BODY = 'UPDATE-NEW-MESSAGE-BODY';
+const SEND_MESSAGE = 'SEND-MESSAGE';
+
+
+const dialogsReducer = (state, action) => {
+
+    switch (action.type) {
+        case UPDATE_NEW_MESSAGE_BODY:
+            state.newMessageBody = action.body;
+            return state;
+        case SEND_MESSAGE:
+            let body = state.newMessageBody;
+            state.newMessageBody = '';
+            state.messages.push({id:7,message: body});
+
+            return state
+        default:
+            return state
+
+    }
+
+
+    return state;
+}
+
+export default dialogsReducer;
+
+
+
+export const sendMessageCreator = ()=>({type:SEND_MESSAGE});
+export  const updateNewMessageBodyCreator = (body)=>({
+    type: UPDATE_NEW_MESSAGE_BODY,
+    body: body
+})
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,7 +1,6 @@
-const ADD_POST = 'ADD-POST';
-const UPDATE_NEW_POST_TEXT = "UPDATE-NEW-POST_TEXT";
-const UPDATE_NEW_MESSAGE_BODY = 'UPDATE-NEW-MESSAGE-BODY';
-const SEND_MESSAGE = 'SEND-MESSAGE';
+import profileReducer from "./profile-reducer";
+import dialogsReducer from "./dialogs-reducer";
+
 
 
 let store = {
@@ -46,44 +45,37 @@ let store = {
         this._callSubscriber = observer;
     },
     dispatch(action) {
-        if (action.type === "ADD-POST") {
 
-            let newPost = {
-                id: 5,
-                message: this._state.profilePage.newPostText,
-                likesCount: 0
-            }
-            this._state.profilePage.posts.push(newPost)
-            this._state.profilePage.newPostText = '';
-            this._callSubscriber(this._state)
-        } else if (action.type === "UPDATE-NEW-POST_TEXT") {
-            this._state.profilePage.newPostText = action.newText;
-            this._callSubscriber(this._state)
-        } else if (action.type === UPDATE_NEW_MESSAGE_BODY) {
-            this._state.dialogsPage.newMessageBody = action.body;
-            this._callSubscriber(this._state)
-        } else if (action.type === SEND_MESSAGE) {
-           let body= this._state.dialogsPage.newMessageBody;
-            this._state.dialogsPage.newMessageBody='';
-            this._state.dialogsPage.messages.push({id:7,message: body});
-            this._callSubscriber(this._state)
-        }
+        this._state.profilePage=profileReducer(this._state.profilePage, action);
+        this._state.dialogsPage=dialogsReducer(this._state.dialogsPage, action);
+        this._callSubscriber(this._state)
+        // if (action.type === "ADD-POST") {
+        //
+        //     let newPost = {
+        //         id: 5,
+        //         message: this._state.profilePage.newPostText,
+        //         likesCount: 0
+        //     }
+        //     this._state.profilePage.posts.push(newPost)
+        //     this._state.profilePage.newPostText = '';
+        //     this._callSubscriber(this._state)
+        // } else if (action.type === "UPDATE-NEW-POST_TEXT") {
+        //     this._state.profilePage.newPostText = action.newText;
+        //     this._callSubscriber(this._state)
+        // } else if (action.type === UPDATE_NEW_MESSAGE_BODY) {
+        //     this._state.dialogsPage.newMessageBody = action.body;
+        //     this._callSubscriber(this._state)
+        // } else if (action.type === SEND_MESSAGE) {
+        //    let body= this._state.dialogsPage.newMessageBody;
+        //     this._state.dialogsPage.newMessageBody='';
+        //     this._state.dialogsPage.messages.push({id:7,message: body});
+        //     this._callSubscriber(this._state)
+        // }
     }
 
 }
 
 
-export const addPostActionCreator = () => ({type: ADD_POST});
 
-export const updateNewPostTextActionCreator = (text) => ({
-    type: UPDATE_NEW_POST_TEXT,
-    newText: text
-});
-
-export const sendMessageCreator = ()=>({type:SEND_MESSAGE});
-export  const updateNewMessageBodyCreator = (body)=>({
-    type: UPDATE_NEW_MESSAGE_BODY,
-    body: body
-})
 
 export default store;
